e7c7e715497f9de46e05765d96ff98c17e82f775
coxjoseph <coxjoseph4004@gmail.com>
2024-10-03 14:50:17 -0400

    refactor with base path declaration

--- App/sdm.py
+++ App/sdm.py
@@ -1,18 +1,22 @@
 import sys
 import os
-
-# Determine the base path depending on whether the script is bundled by PyInstaller
-if getattr(sys, 'frozen', False):
-    # If bundled, use sys._MEIPASS as the base path
-    base_path = sys._MEIPASS
-else:
-    # If running normally, use the current directory
-    base_path = os.path.abspath(os.path.dirname(__file__))
-
-# Append the directory containing your modules to sys.path
-print(base_path)
-sys.path.append(base_path)
-
 from sdm_user_interface import SkynDataManagerApp
 
-SkynDataManagerApp()
+
+def setup_base_path() -> str:
+    if getattr(sys, 'frozen', False):
+        # If bundled by PyInstaller, use sys._MEIPASS as the base path
+        return getattr(sys,'._MEIPASS', os.getcwd())
+    else:
+        # If running normally, use the current directory
+        return os.path.abspath(os.path.dirname(__file__))
+
+
+if __name__ == '__main__':
+    base_path = setup_base_path()
+
+    # TODO: configure and create logging
+    print(f"Running from base path {base_path}")
+    sys.path.append(base_path)
+
+    SkynDataManagerApp()
